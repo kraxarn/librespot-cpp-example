24f0d4a87cb2f1735e8a84d9a4eb393a7f04b3ec
kraxarn <me@kraxarn.com>
2021-02-07 11:32:36 +0100

    Testing connection

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,25 @@
 
 int main(int argc, char **argv)
 {
-	auto core = rs::tokio::core::core();
-
+	if (argc < 3)
+	{
+		std::cout << "usage: librespot-app <username> <password>" << std::endl;
+		return 0;
+	}
 	auto cfg = rs::core::config::session_config();
 	auto cred = rs::core::authentication::with_password(argv[1], argv[2]);
 
 	std::cout << "connecting..." << std::endl;
-	auto session = rs::core::session::connect(cfg, cred, core);
+
+	try
+	{
+		auto session = rs::core::session::connect(cfg, cred);
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+		return 1;
+	}
 
 	return 0;
 }
